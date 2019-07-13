51b1a1ab568259b5db2edfaee01761f337379ffd
joeyrosztoczy <joey@wbear.co>
2019-07-13 16:57:48 -0600

    bitly turing training

--- lib/biteme/cache.ex
+++ lib/biteme/cache.ex
@@ -1,6 +1,8 @@
 defmodule Biteme.Cache do
   use GenServer
 
+  alias Biteme.Shortener
+
   # Client API
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
@@ -12,12 +14,21 @@ defmodule Biteme.Cache do
 
   # Server Implentation
   def init(state) do
-    {:ok, state}
+    {:ok, state, {:continue, :load_urls}}
   end
 
   def handle_call({:insert_url, url}, _from, state) do
-    IO.puts("SUP DUDE")
-    IO.inspect(url)
+    ConCache.put(:slug_cache, url.id, url.original_url)
     {:reply, {:ok, url}, state}
+  end
+
+  def handle_continue(:load_urls, state) do
+    urls = Shortener.list_urls()
+
+    Enum.each(urls, fn url ->
+      ConCache.put(:slug_cache, url.id, url.original_url)
+    end)
+
+    {:noreply, state}
   end
 end
