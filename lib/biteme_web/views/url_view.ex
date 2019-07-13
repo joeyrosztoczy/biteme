b9b7662572242c4c9177c3437faf87f356bbbeb3
joeyrosztoczy <joey@wbear.co>
2019-07-13 11:32:17 -0600

    save work

--- lib/biteme_web/views/url_view.ex
+++ lib/biteme_web/views/url_view.ex
@@ -7,11 +7,14 @@ defmodule BitemeWeb.UrlView do
   end
 
   def render("show.json", %{url: url}) do
+    short_url = Base62.encode(url.id)
+    url = Map.from_struct(url)
+    url = Map.put(url, :short_url, short_url)
+    IO.inspect(url)
     %{data: render_one(url, UrlView, "url.json")}
   end
 
   def render("url.json", %{url: url}) do
-    %{id: url.id,
-      original_url: url.original_url}
+    %{id: url.id, original_url: url.original_url, short_url: url.short_url}
   end
 end
